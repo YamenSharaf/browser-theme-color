3dd1fecf1e64a7a15bed47f250a90e1cac0dce7a
Yamen Sharaf <YamenSharaf@Gmail.com>
2017-03-31 11:53:38 +0200

    modular

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
-$("#element-id").waypoint(function() {
-    //Do something
-	confirm("Press a button!");
-  }
-}, { offset: '100%'});
+$('#click-android').on('click', function() {
+    // Just replacing the value of the 'content' attribute will not work.
+    $('meta[name=theme-color]').remove();
+    $('head').append( '<meta name="theme-color" content="#EF6C00">' );
+});
